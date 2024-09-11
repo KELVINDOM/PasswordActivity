3643f188e82a6171c50a8fd01f941e014429a07d
KELVINDOM <93261549+KELVINDOM@users.noreply.github.com>
2024-09-11 18:22:50 +0700

    Update pass

--- pass.php
+++ pass.php
@@ -1,3 +1,1 @@
-user.equals("BILLIONZ GAMING") && pass.equals("2005")
-						|| user.equals("SCORPIXX") && pass.equals("1616")
-						|| user.equals("MY CHANNEL") && pass.equals("1234")
+zitJS8GHRW4K0VF
